b9653a237ece24ac64f6434fe3749a1933c8eb83
Pathao Ltd <pathaoltd@Pathaos-MacBook-Air-2.local>
2024-12-30 15:18:24 +0600

    changed swift language version

--- Package@swift-5.9.swift
+++ Package@swift-5.9.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.4
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -11,12 +11,13 @@ let package = Package(
         .tvOS(.v12),
         .visionOS(.v1)
     ],
-
+    
     products: [
         .library(name: "PagerView", targets: ["PagerView"]),
         .library(name: "PagerRx", targets: ["PagerRx"]),
         .library(name: "PagerRxDynamic", type: .dynamic, targets: ["PagerRx"]),
     ],
+    
     
     dependencies: [
 //        .package(path: "../ScrollShadow"),
@@ -24,7 +25,6 @@ let package = Package(
         .package(url: "https://github.com/Mubin-pathao/ZoomableImageView.git", .upToNextMajor(from: "1.0.0")),
         .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "6.0.0"))
     ],
-    
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
@@ -37,7 +37,15 @@ let package = Package(
             resources: [
                 .process("../PrivacyInfo.xcprivacy")
             ]
+//            ,
+//            swiftSettings: [
+//                .enableExperimentalFeature(<#T##name: String##String#>),
+//                .enableUpcomingFeature(<#T##name: String##String#>, .when(platforms: <#T##[Platform]#>, configuration: <#T##BuildConfiguration#>)),
+//                .interoperabilityMode(<#T##mode: SwiftSetting.InteroperabilityMode##SwiftSetting.InteroperabilityMode#>, <#T##condition: BuildSettingCondition?##BuildSettingCondition?#>),
+//                .unsafeFlags(<#T##flags: [String]##[String]#>, <#T##condition: BuildSettingCondition?##BuildSettingCondition?#>)
+//            ]
         ),
+        
         .target(name: "PagerRx",
                 dependencies: [
                     "PagerView",
